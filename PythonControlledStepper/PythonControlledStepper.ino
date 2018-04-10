f2c0465afa1eb4abb7fc423a9bf78b15603394e1
callinSwitzer <callin.switzer@gmail.com>
2018-04-10 16:51:06 -0700

    started nectar learning

--- PythonControlledStepper/PythonControlledStepper.ino
+++ PythonControlledStepper/PythonControlledStepper.ino
@@ -20,27 +20,51 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // to motor port #2 (M3 and M4)
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
+// define variable we'll read from python
+int val = 0;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Stepper test!");
-
   AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(5000);  // OR with a different frequency, say 1KHz
-  
-  myMotor->setSpeed(100);  // 10 rpm   
+  myMotor->setSpeed(100);  //  rpm   
+  Serial.println("Setup complete"); 
 }
 
 void loop() {
+
+  // read serial port
+  val = Serial.read();
+
+  // 102 = f
+   if (val == 102){
+    Serial.println(val);
+    myMotor->step(40, FORWARD, SINGLE);    
+    // release motor so it doesn't get too hot!
+    myMotor->release(); 
+  }
+
+  // 98 = b
+  else if (val == 98) {Serial.println(val);
+    myMotor->step(40, BACKWARD, SINGLE);    
+    // release motor so it doesn't get too hot!
+    myMotor->release();  
+    }
+
+  
   //Serial.println("Single coil steps");
   //myMotor->step(500, FORWARD, SINGLE); 
   //myMotor->step(500, BACKWARD, SINGLE); 
   //myMotor-> release(); 
-  delay(1000); 
-
-  //Serial.println("Double coil steps");
-  myMotor->step(200, FORWARD, DOUBLE); 
-  myMotor->step(200, BACKWARD, DOUBLE);
+//  delay(3000); 
+//
+//  Serial.println("running!"); 
+//
+//  //Serial.println("Double coil steps");
+//  myMotor->step(200, FORWARD, DOUBLE); 
+//  myMotor->step(200, BACKWARD, DOUBLE);
+//  
+//  // release so motor doesn't get too hot
+//  myMotor->release();
   
   //Serial.println("Interleave coil steps");
   //myMotor->step(100, FORWARD, INTERLEAVE); 
