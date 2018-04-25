70c67c357a948d1493fff2be7f55965e4f53842d
callinSwitzer <callin.switzer@gmail.com>
2018-04-25 13:36:52 -0700

    added more arduino

--- photoDetector_andStepper/photoDetector_andStepper.ino
+++ photoDetector_andStepper/photoDetector_andStepper.ino
@@ -15,6 +15,7 @@ int lightPin = A0;  //define a pin for Photo resistor
 int lightPin1 = A3;  //define a pin for Photo resistor
 int lightPin2 = A5;  //define a pin for Photo resistor
 int stepIn; 
+int stepIn_bottom; 
 int ar1; 
 int ar2; 
 int ar3; 
@@ -33,27 +34,35 @@ void loop() {
 
   // put your main code here, to run repeatedly:
   stepIn = digitalRead(7); 
-  Serial.print(stepIn *200 + 500); 
+  Serial.print(stepIn *10 + 30); 
   Serial.print(","); 
+  stepIn_bottom = digitalRead(6); 
+  Serial.print(stepIn_bottom *10 + 30); 
+  Serial.print(","); 
+  
+  
 
-  // 102 = f  
+   // 98 = b
    if (val == 98){
     Serial.println(val);
-    myMotor->step(40, FORWARD, SINGLE);    
-    // release motor so it doesn't get too hot!
-    myMotor->release(); 
+      moveBackward();
   }
 
-  // 98 = b
-  else if (val == 102) {Serial.println(val);
-    myMotor->step(40, BACKWARD, SINGLE);    
-    // release motor so it doesn't get too hot!
-    myMotor->release();  
+ 
+  // 102 = f  
+  else if (val == 102) {
+    Serial.println(val);
+    moveForward();
     }
 
-//  ar1 = analogRead(A2);
-//  Serial.print(ar1);
-//  Serial.print(",");
+  // 99 = c
+  else if (val == 99 && stepIn < 1) {
+    centerup();
+    }
+
+  ar1 = analogRead(A2);
+  Serial.print(ar1);
+  Serial.print(",");
 //  //delay(10); 
   ar2 = analogRead(A0); 
   //delay(50); 
@@ -65,3 +74,53 @@ void loop() {
   Serial.println(ar3); 
 
 }
+
+
+// This moves the punger all the way to the back, and then forward a little bit
+void centerup()
+{
+  stepIn = digitalRead(7); 
+ 
+  while(stepIn < 1){
+ 
+   myMotor->step(10, FORWARD, SINGLE);    
+    // release motor so it doesn't get too hot!
+   myMotor->release();
+  stepIn = digitalRead(7); 
+  }
+
+  delay(100); 
+  //move forward a little bit
+  for (int i=0; i <= 10; i++){
+      moveForward();
+   }
+  
+}
+
+
+
+// This function moves forward
+void moveForward()
+{
+  stepIn_bottom = digitalRead(6); 
+  //move forward a little bit
+  if (stepIn_bottom < 1){
+   myMotor->step(40, BACKWARD, SINGLE);    
+    // release motor so it doesn't get too hot!
+   myMotor->release();
+  }
+}
+
+
+// This function moves backward
+void moveBackward()
+{
+  stepIn = digitalRead(7); 
+  //move forward a little bit
+  if (stepIn < 1){
+   myMotor->step(40, FORWARD, SINGLE);    
+    // release motor so it doesn't get too hot!
+   myMotor->release();
+  }
+}
+
